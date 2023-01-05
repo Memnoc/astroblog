8ba8a9c0ef92b8b0915b4e7e23c83ce035d1bd33
Memnoc <Memnoc>
2023-01-05 08:35:49 +0000

    adding js to create dynamic content

---
+++ src/scripts/menu.js
@@ -0,0 +1,5 @@
+// attaching listeners to hamburger menu
+document.querySelector(".hamburger").addEventListener("click", () => {
+  document.querySelector(".nav-links").classList.toggle("expanded");
+});
+

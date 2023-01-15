829298587b6f7dda5be6f99ada3ba8d1d20a0112
Memnoc <Memnoc>
2023-01-15 11:08:54 +0000

    nuked home page and adopted tailwindcss

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {},
+  },
+  plugins: [],
+}

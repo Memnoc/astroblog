7472fa4dbf5a4f6a74956b852b430129aa1283b6
Memnoc <smartdroidesign@gmail.com>
2023-01-24 12:06:50 +0000

    imported Nord palette as variables and styled Home

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -4,14 +4,15 @@ import { useState } from 'preact/hooks';
 export default function Greeting({ messages }) {
 
   const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
-
   const [greeting, setGreeting] = useState(randomMessage());
+
 
   return (
     <div>
       <h3>{greeting}! Thank you for visiting!</h3>
-      <button onClick={() => setGreeting(randomMessage())}>
-        New Greeting
+      <br />
+      <button class="greeting-button bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={() => setGreeting(randomMessage())}>
+        Change greeting!
       </button>
     </div>
   );
